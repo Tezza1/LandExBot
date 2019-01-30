86fe9508b54271530083228f28735cd2c7228ba7
Tezza1 <tshenker@gmail.com>
2019-01-31 04:15:51 +0900

    changed file structure

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const gravatar = require('gravatar');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -41,6 +42,16 @@ app.get('/', (req, res) => {
 // Use routes
 app.use('/users', users);
 app.use('/dialogs', dialogs);
+
+// If in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
