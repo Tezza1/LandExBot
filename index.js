f964219506d02767dec4b53323fd728f0725e153
Terry Shenker <tshenker@gmail.com>
2019-02-04 12:10:01 +0900

    added checking for google key

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const languageCode = process.env.DF_LANGUAGE_CODE;
 
 const credentials = {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
 };
 
 // Create a new session
